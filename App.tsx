91239a0b062711b1ff6725a0742d17c93bdce780
Aideed <mwangaaideed@gmail.com>
2024-04-18 17:30:42 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import loginScreen from './src/screens/loginScreen';
+import { Text, View } from 'react-native';
+export default function App(){
+  return (
+    <View>
+      <Text>Hello there</Text>
+    </View>
+  );
+}
+
+
---
+++ src/screens/loginScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {colors} from '../assets/customStyles';
+export default function loginScreen() {
+  return (
+    <div>loginScreen</div>
+  )
+}
+
+
